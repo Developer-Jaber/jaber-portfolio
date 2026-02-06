a34ba6d88a04d1541e6366c810597c7ee6d71d24
Jaber Hossain <Sanchi Gyan>
2026-02-06 23:09:10 +0400

    feat: add simple navbar

---
+++ src/components/layout/navbar/NavLinks.tsx
@@ -0,0 +1,7 @@
+export const navLinks = [
+    { label: "Home", href: "/" },
+    { label: "About", href: "#about" },
+    { label: "Projects", href: "#projects" },
+    { label: "Experience", href: "#experiance" },
+    { label: "Contact", href: "#contact" }
+]
